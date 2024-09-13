ab747d56eae11b4445c1b75680055925396fb0db
Walter Diaz <nachowd@gmail.com>
2024-09-13 11:02:17 -0300

    agregue el itemlistcontainer

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
 const App = () => {
   return (
     <ChakraProvider>
       <NavBar />
+      <ItemListContainer greeting="Hola Profe, tenga usted buenos dias, buenas tardes y buenas noches!" />
     </ChakraProvider>
   );
 };
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,8 @@
+const ItemListContainer = ({greeting}) => {
+    return (
+        <div style={{fontFamily: "Calibri", fontSize: "25px", color: "Green"}}>{greeting}</div>
+    )
+    
+};
+
+export default ItemListContainer; 
