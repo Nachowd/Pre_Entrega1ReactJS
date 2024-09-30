87dd1e7ee91bdbebe7d1266ee97c848e5bb6cf80
Walter Diaz <nachowd@gmail.com>
2024-09-30 01:23:05 -0300

    2da preEntrega para entregar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-
 import { ChakraProvider } from "@chakra-ui/react";
-
-import NavBar from "./components/NavBar/NavBar";
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-
+import MainLayout from "./layouts/MainLayout";
+import { MainRouter } from "./router";
 const App = () => {
   return (
     <ChakraProvider>
-      <NavBar />
-      <ItemListContainer greeting="Hola Profe, tenga usted buenos dias, buenas tardes y buenas noches!" />
+      <MainLayout>
+        <MainRouter />
+      </MainLayout>
     </ChakraProvider>
   );
 };
--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,20 +1,18 @@
 import { CiShoppingCart } from "react-icons/ci";
 
-const CartWidget = () => {
-return (
-    <div
-    style={{
+export const CartWidget = () => {
+    return (
+        <div
+        style={{
         display: "flex",
-        marginRight: "1.25rem",
+        marginRight: "20px",
         alignItems: "center",
         width: "30%",
         justifyContent: "space-between",
-    }}
+        }}
     >
-    <CiShoppingCart size={30} />
-    10
-    </div>
-);
+        <CiShoppingCart size={30} />
+        10
+        </div>
+    );
 };
-
-export default CartWidget;
